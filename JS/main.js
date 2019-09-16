8599ac263f2dc6ed890d3cfed0e0a31411917083
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-15 22:04:44 -0400

    Foursquare API Info

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+// Foursquare API Info
+const clientId = 'OUK1JT1GIAJBIKL4V5GLUHXDSKBGH3U2S0343HXGNQB54TGG';
+const clientSecret = 'YK5JFQUI3MUFBTFWLWAYUQSIKNCKZKIV3QQ4IK43Z3KTVFTY';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';

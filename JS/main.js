93f6510883a94cda1a5b3cc4da3f1bd5e374234a
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-15 22:06:28 -0400

    Weatherstack API Info

--- JS/main.js
+++ JS/main.js
@@ -2,3 +2,7 @@
 const clientId = 'OUK1JT1GIAJBIKL4V5GLUHXDSKBGH3U2S0343HXGNQB54TGG';
 const clientSecret = 'YK5JFQUI3MUFBTFWLWAYUQSIKNCKZKIV3QQ4IK43Z3KTVFTY';
 const url = 'https://api.foursquare.com/v2/venues/explore?near=';
+
+// Weatherstack API Info
+const apiKey = '99c499c1f2fa61a2fb4252a557d4c8d3';
+const forecastUrl = 'https://api.weatherstack.com/forecast?access_key=';

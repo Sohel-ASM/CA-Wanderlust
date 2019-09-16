640e45e1f9a85b0a4053f564fbee314ed2f72c86
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-15 22:12:42 -0400

    const getVenues = async () => {...}

--- JS/main.js
+++ JS/main.js
@@ -15,3 +15,21 @@ const $container = $('.container');
 const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
 const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4")];
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// Add AJAX functions here:
+const getVenues = async () => {
+    const city = $input.val();
+    const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20190914`;
+    try {
+        const response = await fetch(urlToFecth);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+            const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+            return venues;
+        }
+        throw new Error('Request failed!');
+    } catch (error) {
+        console.log(error);
+    }
+}

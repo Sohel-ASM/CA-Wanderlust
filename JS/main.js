a5e96dbfc96ff2df4354c94e262ed76cf6d3eb5a
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-15 22:07:28 -0400

    Page Elements

--- JS/main.js
+++ JS/main.js
@@ -6,3 +6,12 @@ const url = 'https://api.foursquare.com/v2/venues/explore?near=';
 // Weatherstack API Info
 const apiKey = '99c499c1f2fa61a2fb4252a557d4c8d3';
 const forecastUrl = 'https://api.weatherstack.com/forecast?access_key=';
+
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
+const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4")];
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
